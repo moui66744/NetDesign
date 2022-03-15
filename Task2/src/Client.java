41360681cbdd9f7fc26d45c8606c1ebe249cddc9
moui66744 <whaoyu09@163.com>
2022-03-15 15:21:15 +0800

    添加注释

--- Task2/src/Client.java
+++ Task2/src/Client.java
@@ -1,6 +1,19 @@
+/**
+ * @author: why(moui), USTB
+ * @time: 2022-03-15
+ * @project: FPVGA
+ * TELNET program Client based on TCP protocol
+ * This project implements a simple FPGA emulator
+ * The client does not need to install the Verilog runtime environment,
+ * just upload the *.v file to the server
+ * and then change the switch to display the Verilog execution result by LED
+ * */
+
 import javax.swing.*;
 import javax.swing.filechooser.FileNameExtensionFilter;
 import java.awt.*;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 import java.awt.event.WindowEvent;
 import java.awt.event.WindowListener;
 import java.io.*;
@@ -11,42 +24,47 @@ import java.util.Scanner;
 import static java.lang.Thread.sleep;
 
 public class Client implements Runnable {
+    // Java GUI component
     public JFrame frame;
     public JPanel panel;
     public JPanel panel1;
     public JPanel panel2;
     public JPanel panel3;
-    public JLabel[] LEDs;
-    public JToggleButton[] toggleBtns = new JToggleButton[16];
-    public JButton chooseFileBtn;
-    public JToggleButton runBtn;
-    public JButton openTerminalBtn;
-    public File filePath = new File("C:\\Users\\86187\\Desktop\\a.v");
-    public int[] switchStatus = new int[16];
-
-    public Thread runThread;
-    public boolean isRun;
-    public boolean enableRun;
-
+    public JLabel[] LEDs;                           // 16 LEDs of fpvga
+    public JToggleButton[] switches;                // 16 switches of fpvga
+    public JLabel rst;                              // rst btn of fpvga
+    public JButton chooseFileBtn;                   // choose file uploaded to Server
+    public JToggleButton runBtn;                    // run fpvga
+    public JButton openTerminalBtn;                 // open terminal
+    public File filePath = new File(""); // Client file path of *.v file
+    public int[] switchStatus = new int[16];        // record switch status
+    // icon and font
     public ImageIcon LEDOnIcon;
     public ImageIcon LEDOffIcon;
     public ImageIcon switchOnIcon;
     public ImageIcon switchOffIcon;
+    public ImageIcon rstOnIcon;
+    public ImageIcon rstOffIcon;
     public ImageIcon runIcon;
     public ImageIcon terminalIcon;
     public ImageIcon uploadIcon;
     public Font font;
-
+    //
+    public Thread runThread;                        // another thread to run fpvga
+    public boolean isRun;                           // runThread whether to continue
+    public boolean enableRun;                       // to enable runBtn. If *.v file compilation error, enableRun = false
+    public boolean isRst;                           // *.v file rst signal
+    // socket
     public Socket socket;
-    public PrintWriter sender;
-    public BufferedReader recver;
+    public PrintWriter sender;                      // send buffer
+    public BufferedReader recver;                   // receive buffer
 
     Client() throws IOException {
-        frame = new JFrame("FPGA模拟器");
-        frame.setSize(1000, 400);
-        frame.setLocationRelativeTo(null);
+        frame = new JFrame("FPVGA");        // title
+        frame.setSize(1000, 400);  // window size 1000 * 400
+        frame.setLocationRelativeTo(null);          // at the center of the screen
         frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-
+        // set layout
         panel = new JPanel(new GridLayout(3, 1));
         panel.setBackground(Color.white);
         panel1 = new JPanel(new GridLayout(0, 16));
@@ -55,42 +73,72 @@ public class Client implements Runnable {
         panel2.setBackground(Color.white);
         panel3 = new JPanel(new FlowLayout());
         panel3.setBackground(Color.white);
-
+        // init icon and font
         LEDOnIcon = new ImageIcon("resources/LED_on.png");
         LEDOffIcon = new ImageIcon("resources/LED_off.png");
         switchOnIcon = new ImageIcon("resources/switch_on.png");
         switchOffIcon = new ImageIcon("resources/switch_off.png");
+        rstOnIcon = new ImageIcon("resources/rst_on.png");
+        rstOffIcon = new ImageIcon("resources/rst_off.png");
         runIcon = new ImageIcon("resources/run.png");
         terminalIcon = new ImageIcon("resources/terminal.png");
         uploadIcon = new ImageIcon("resources/terminal.png");
         font = new Font("console", Font.BOLD, 14);
 
-        // 创建LED
+        // create LED
         LEDs = new JLabel[16];
         for (int i=0; i<16; i++) {
             LEDs[i] = new JLabel();
             LEDs[i].setIcon(LEDOffIcon);
             panel1.add(LEDs[i]);
         }
-        // 创建开关
+        // create switch
+        switches = new JToggleButton[16];
         for (int i=0; i<16; i++) {
-            toggleBtns[i] = new JToggleButton();
-            // 首先设置不绘制按钮边框
-            toggleBtns[i].setBorderPainted(false);
-            toggleBtns[i].setFocusPainted(false);
-            toggleBtns[i].setContentAreaFilled(false);
-            toggleBtns[i].setBackground(Color.white);
-            toggleBtns[i].setSelectedIcon(switchOnIcon);
-            toggleBtns[i].setIcon(switchOffIcon);
-            // 添加 toggleBtns 的状态被改变的监听
-            toggleBtns[i].addActionListener(e -> {
+            switches[i] = new JToggleButton();
+            switches[i].setBorderPainted(false);
+            switches[i].setFocusPainted(false);
+            switches[i].setContentAreaFilled(false);
+            switches[i].setBackground(Color.white);
+            switches[i].setSelectedIcon(switchOnIcon);
+            switches[i].setIcon(switchOffIcon);
+            // add listener
+            switches[i].addActionListener(e -> {
                 JToggleButton toggleBtn1 = (JToggleButton) e.getSource();
-                switchStatus[Arrays.asList(toggleBtns).indexOf(toggleBtn1)] = toggleBtn1.isSelected() ? 1 : 0;
+                switchStatus[Arrays.asList(switches).indexOf(toggleBtn1)] = toggleBtn1.isSelected() ? 1 : 0;// change switch icon
             });
-            panel2.add(toggleBtns[i]);
+            panel2.add(switches[i]);
         }
 
-        // 上传文件按钮
+        // add reset
+        isRst = false;
+        rst = new JLabel();
+        rst.setIcon(rstOffIcon);
+        // add mouse listener
+        rst.addMouseListener(new MouseListener() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+            }
+            @Override
+            public void mousePressed(MouseEvent e) {// Rst enables when the mouse is pressed
+                isRst = true;
+                rst.setIcon(rstOnIcon);
+            }
+            @Override
+            public void mouseReleased(MouseEvent e) {// Rst disables when the mouse is released
+                isRst = false;
+                rst.setIcon(rstOffIcon);
+            }
+            @Override
+            public void mouseEntered(MouseEvent e) {
+            }
+            @Override
+            public void mouseExited(MouseEvent e) {
+            }
+        });
+        panel3.add(rst);
+
+        // upload file button
         chooseFileBtn = new JButton("choose file");
         chooseFileBtn.setFont(font);
         chooseFileBtn.setPreferredSize(new Dimension(140, 100));
@@ -98,26 +146,24 @@ public class Client implements Runnable {
         chooseFileBtn.setIcon(uploadIcon);
         chooseFileBtn.setHorizontalTextPosition(SwingConstants.CENTER);
         chooseFileBtn.setVerticalTextPosition(SwingConstants.BOTTOM);
+        // listener
         chooseFileBtn.addActionListener(e -> {
-            // 创建一个默认的文件选取器
             JFileChooser fileChooser = new JFileChooser();
-            // 设置文件选择的模式（只选文件、只选文件夹、文件和文件均可选）
             fileChooser.setFileSelectionMode(JFileChooser.FILES_ONLY);
-            // 设置是否允许多选
+            // single file
             fileChooser.setMultiSelectionEnabled(false);
-            // 设置默认使用的文件过滤器
+            // file filter: *.v
             fileChooser.setFileFilter(new FileNameExtensionFilter("verilog(*.v)", "v"));
-            // 打开文件选择框（线程将被阻塞, 直到选择框被关闭）
             int result = fileChooser.showOpenDialog(frame);
             if (result == JFileChooser.APPROVE_OPTION) {
-                // 如果点击了"确定", 则获取选择的文件路径
+                // get file path
                 filePath = fileChooser.getSelectedFile();
                 uploadFile();
             }
         });
         panel3.add(chooseFileBtn);
 
-        // 运行按钮
+        // run button
         runBtn = new JToggleButton("run");
         runBtn.setFont(font);
         runBtn.setPreferredSize(new Dimension(140, 100));
@@ -125,6 +171,7 @@ public class Client implements Runnable {
         runBtn.setIcon(runIcon);
         runBtn.setHorizontalTextPosition(SwingConstants.CENTER);
         runBtn.setVerticalTextPosition(SwingConstants.BOTTOM);
+        // listener
         runBtn.addActionListener(e -> {
             JToggleButton runBtn = (JToggleButton) e.getSource();
             if (runBtn.isSelected()) {
@@ -139,7 +186,7 @@ public class Client implements Runnable {
         });
         panel3.add(runBtn);
 
-        // 打开终端按钮
+        // open terminal button
         openTerminalBtn = new JButton("open terminal");
         openTerminalBtn.setFont(font);
         openTerminalBtn.setPreferredSize(new Dimension(140, 100));
@@ -159,10 +206,11 @@ public class Client implements Runnable {
             public void windowOpened(WindowEvent e) {
             }
             @Override
-            public void windowClosing(WindowEvent e) {
+            public void windowClosing(WindowEvent e) {// When the window closes, the command "shutdown" is sent to Server
                 try {
                     sender.print("shutdown");
                     sender.flush();
+                    // close connection
                     sender.close();
                     recver.close();
                     socket.close();
@@ -189,50 +237,64 @@ public class Client implements Runnable {
         frame.setContentPane(panel);
         frame.setVisible(true);
 
+        // connect with Server
         socket = new Socket("192.168.251.169", 6000);
         sender = new PrintWriter(socket.getOutputStream());
         recver = new BufferedReader(new InputStreamReader(socket.getInputStream()));
         System.out.println("connect successfully!");
-//        openTerminal();
     }
 
+    /**
+     * open terminal
+     * ready to receive command from user
+     * send command to Server
+     * and receive result from Server
+     * */
     public void openTerminal() {
         try {
             Scanner scan = new Scanner(System.in);
             while (true) {
                 System.out.print(">");
                 String cmd = scan.nextLine();
-                if (cmd.contains("exit")) {
+                if (cmd.contains("exit")) {// If the user enters "exit", close terminal
                     break;
                 }
-                sender.print(cmd);
+                sender.print(cmd);// send command to Server
                 sender.flush();
                 StringBuilder result = new StringBuilder();
+                // receive from Server line by line
                 while (true) {
                     String line = recver.readLine();
-                    if (line.contains("ERESP")) break;
+                    if (line.contains("ERESP")) break;// Server sends "ERESP" indicating that there are no more results to receive
                     result.append(line).append("\n");
                 }
-                System.out.println(result);
+                System.out.println(result);// print all results
             }
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
+    /**
+     * set LED icon according to LEDStatus
+     * */
     public void setLEDStatus(int[] LEDStatus) {
         for (int i=0; i<16; i++) {
             LEDs[i].setIcon(LEDStatus[i] == 1 ? LEDOnIcon : LEDOffIcon);
         }
     }
 
+    /**
+     * Upload the file to the server
+     * compile the file
+     * and receive the compilation information
+     * */
     public void uploadFile() {
         try {
-            StringBuilder cmd = new StringBuilder("upload");
-            sender.print(cmd);
+            sender.print("upload");
             sender.flush();
             System.out.println(recver.readLine());
-            // 逐行发送文件
+            // upload file line by line
             Scanner fileScan = new Scanner(new FileReader(filePath));
             while (fileScan.hasNextLine()) {
                 String line = fileScan.nextLine();
@@ -240,56 +302,80 @@ public class Client implements Runnable {
                 sender.print(line + "\n");
                 sender.flush();
             }
-            sender.print("EREQ");
+            sender.print("EREQ");// Client Send "EREQ" indicating that there are no more data to send
             sender.flush();
             System.out.println("recv: " + recver.readLine());
-
+            // send command to compile the file
             sender.print("make vmodel");
             sender.flush();
             StringBuilder result = new StringBuilder();
+            enableRun = true;
+            // receive the compilation information line by line
             while (true) {
                 String line = recver.readLine();
-                if (line.contains("ERESP")) break;
+                if (line.contains("error")) enableRun = false;// Whether there is an error in the code
+                if (line.contains("ERESP")) break;// Server sends "ERESP" indicating that there are no more results to receive
                 result.append(line).append("\n");
             }
+            runBtn.setEnabled(enableRun);// enable/disable run button
             System.out.println(result);
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
+    /**
+     * another thread to run FPVGA
+     * Use multithreading to ensure that the GUI is not blocked
+     *
+     * Workflow:
+     * Client-------------------------- Server
+     * "fpvga"                  =>
+     *                          <=      Whether to enable FPVGA
+     * LOOP:
+     * clk|rst|switchStatus     =>
+     *                          <=      LED status
+     * goto LOOP
+     * */
     @Override
     public void run() {
         try {
-            StringBuilder cmd = new StringBuilder("fpvga");
-            sender.print(cmd);
+            boolean clk = true;
+            // Client send "fpvga" to Server
+            // to enable fpvge
+            sender.print("fpvga");
             sender.flush();
             System.out.println(recver.readLine());
+            // Client send clk|rst|switchStatus to Server
+            // Server send LEDstatus to Client
             while (true) {
-                if (!isRun) {
-                    StringBuilder ereq = new StringBuilder("EREQ");
-                    sender.print(ereq);
+                clk = !clk;// The clock signal flips automatically
+                if (!isRun) {// End of the run
+                    sender.print("EREQ");// send "EREQ" indicates the end of the FPVGA
                     sender.flush();
                     System.out.println(recver.readLine());
                     break;
                 }
+                // Client send clk|rst|switchStatus to Server
                 StringBuilder switchStatusSequence = new StringBuilder();
                 for (int i=0; i<16; i++) {
                     switchStatusSequence.append(switchStatus[i]);
                 }
-                System.out.println("send switch: " + switchStatusSequence);
-                sender.print(switchStatusSequence);
+                System.out.println(isRst);
+                System.out.println("send: " + (clk ? "1" : "0") + " " + (isRst ? "0" : "1") + " " + switchStatusSequence);
+                sender.print((clk ? "1" : "0") + (isRst ? "0" : "1") + switchStatusSequence);
                 sender.flush();
+                // Server send LEDstatus to Client
                 String LEDStatusSequence = recver.readLine();
                 System.out.println("recv LED: " + LEDStatusSequence);
                 int[] LEDStatus = new int[16];
                 for (int i=0; i<16; i++) {
                     LEDStatus[i] = LEDStatusSequence.charAt(i) - '0';
-                    System.out.print(LEDStatus[i]);//
+                    System.out.print(LEDStatus[i]);
                 }
-                setLEDStatus(LEDStatus);
+                setLEDStatus(LEDStatus);// change LED status
+                // It can be considered that the clock frequency of FPVGA is 10 milliseconds by default
+                // (If the network delay is ignored)
                 sleep(10);
             }
         } catch (Exception ex) {
